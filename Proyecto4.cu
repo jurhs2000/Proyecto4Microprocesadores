0dec3a2f3f774c888cdddc84effe2b63d75dee7b
Julio Herrera <jurhs2000@gmail.com>
2020-11-26 23:52:41 -0600

    nilakantha

---
+++ Proyecto4.cu
@@ -0,0 +1,15 @@
+/*
+*----------------------------------------
+* Por Juan Pablo Pineda 19087
+*     Alejandra Gudiel 19232
+*     Oscar Saravia 19322
+*     Julio Herrera 19402
+*     Andres Emilio Quinto 18288
+* ---------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* CC3056 - Programacion de Microprocesadores - Sección 10
+* Proyecto 4, segunda serie de potencias:
+* Cálculo del valor de PI mediante la serie matemática de Leibniz y la serie de Nilakantha.
+* Uso de streams individuales y simultáneos para cada serie con arreglos de 1000 elementos.
+*----------------------------------------
+*/
